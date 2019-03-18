195864c55e7110f91d21e051548470d58a444d6b
llyyss1988 <llyyss88@163.com>
2019-03-18 19:50:37 +0800

    Update install_chakracore.sh

--- scripts/install_chakracore.sh
+++ scripts/install_chakracore.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+
+echo '---------------------------'
+echo `pwd`
+path=(pwd)
+echo $path
+if [ -d "./ChakraCoreFiless" ]; then
+    echo 'file have ...ChakraCoreFiles'
+    echo 'file have ...ChakraCoreFiles'
+else
+    echo 'file no...'
+    echo 'wget https://aka.ms/chakracore/cc_linux_x64_1_11_1'
+    wget https://aka.ms/chakracore/cc_linux_x64_1_11_1
+    echo 'tar xf cc_linux_x64_1_11_1'
+    tar xf cc_linux_x64_1_11_1
+fi
+echo 'ls ChakraCoreFiles/*'
+ls ChakraCoreFiles/*
+echo '---------------------------'
+ls
+
+echo '-------------------'
+echo './ChakraCoreFiles/bin/ch -?'
+./ChakraCoreFiles/bin/ch -?
+echo '-------------------'
+echo ' ./ChakraCoreFiles/bin/ch --version'
+ ./ChakraCoreFiles/bin/ch --version
+
+
 FILE_NAME="ChakraCore.dll"
 TO_FILE=$1${FILE_NAME}
 FROM_FILE=
